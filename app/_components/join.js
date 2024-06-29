3f725c6f6bdd96abb39fca908541e5d0c0570c7f
mitesh dixit <miteshdixit741@gmail.com>
2024-06-30 05:29:18 +0530

    fourth

--- app/_components/join.js
+++ app/_components/join.js
@@ -1,28 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Join() {
   return (
     <div>
-       <form>
-      <div className="mb-3">
-       
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='example@.com'/>
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-      </div>
-      <div className="mb-3">
-        
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='password' />
-      </div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@domain.com"/>
+          <div id="emailHelp" className="form-text">We&apos;ll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
 
-      <div className="d-flex justify-content-center">
-  <button type="submit" className="w-100 items-center gap-6 text-lg border border-primary-300 px-2 py-2 font-medium mb-2 rounded-pill btn btn-info text-light">
-    Agree and Continue
-  </button>
-</div>
-
-    </form>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="w-100 items-center gap-6 text-lg border border-primary-300 px-2 py-2 font-medium mb-2 rounded-pill btn btn-info text-light">
+            Agree and Continue
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Join
+export default Join;
